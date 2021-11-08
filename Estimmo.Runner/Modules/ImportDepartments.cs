b80a4985404fed3d53a7d2bdbe9d471855518cec
Jamie Hodgson <jamie-hodgson@live.com>
2021-11-08 11:12:03 +0100

    import departments and regions

---
+++ Estimmo.Runner/Modules/ImportDepartments.cs
@@ -0,0 +1,46 @@
+using Estimmo.Data;
+using Estimmo.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Features;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace Estimmo.Runner.Modules
+{
+    public class ImportDepartments : ImportFeatureCollectionModule
+    {
+        private readonly ILogger _log = Log.ForContext<ImportDepartments>();
+        private readonly EstimmoContext _context;
+
+        public ImportDepartments(EstimmoContext context)
+        {
+            _context = context;
+        }
+
+        protected override async Task ParseFeatureCollection(FeatureCollection collection)
+        {
+            foreach (var feature in collection)
+            {
+                var id = feature.Attributes["code"].ToString();
+                var name = feature.Attributes["nom"].ToString();
+
+                var centroid = feature.Geometry.Centroid;
+                var enclosingRegion = await _context.Regions.FirstOrDefaultAsync(r => r.Geometry.Covers(centroid));
+
+                if (enclosingRegion == null)
+                {
+                    continue;
+                }
+
+                _log.Information("Importing department {Id} {Name} in {Region}", id, name, enclosingRegion.Name);
+
+                _context.Departments.Add(new Department
+                {
+                    Id = id, Name = name, RegionId = enclosingRegion.Id, Geometry = feature.Geometry
+                });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Estimmo.Runner/Modules/ImportRegions.cs
@@ -0,0 +1,33 @@
+using Estimmo.Data;
+using Estimmo.Data.Entities;
+using NetTopologySuite.Features;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace Estimmo.Runner.Modules
+{
+    public class ImportRegions : ImportFeatureCollectionModule
+    {
+        private readonly ILogger _log = Log.ForContext<ImportRegions>();
+        private readonly EstimmoContext _context;
+
+        public ImportRegions(EstimmoContext context)
+        {
+            _context = context;
+        }
+
+        protected override async Task ParseFeatureCollection(FeatureCollection collection)
+        {
+            foreach (var feature in collection)
+            {
+                var id = feature.Attributes["code"].ToString();
+                var name = feature.Attributes["nom"].ToString();
+
+                _log.Information("Importing region {Id} {Name}", id, name);
+                _context.Regions.Add(new Region { Id = id, Name = name, Geometry = feature.Geometry });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
