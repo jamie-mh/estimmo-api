75fd70011ae541571444a6af31a5d20569b0accb
Jamie Hodgson <jamie-hodgson@live.com>
2022-03-16 14:26:57 +0100

    remove min length

--- Estimmo.Api/Controllers/PlacesController.cs
+++ Estimmo.Api/Controllers/PlacesController.cs
@@ -37,7 +37,7 @@ namespace Estimmo.Api.Controllers
         [SwaggerResponse(StatusCodes.Status200OK, "Place list", typeof(IEnumerable<SimplePlace>))]
         [SwaggerResponse(StatusCodes.Status400BadRequest, "Validation failed")]
         public async Task<ActionResult> GetPlaces(
-            [MinLength(3)] string name, double? latitude, double? longitude, [Range(1, 100)] int limit = 100)
+            string name, double? latitude, double? longitude, [Range(1, 100)] int limit = 100)
         {
             if (name == null && (latitude == null || longitude == null))
             {
