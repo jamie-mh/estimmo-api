e504dd2f46ae3efc36a79c89eebe8cf4904126b2
Jamie Hodgson <jamie-hodgson@live.com>
2022-03-21 10:09:59 +0100

    use properties from simple message

--- Estimmo.Api/Entities/DetailedMessage.cs
+++ Estimmo.Api/Entities/DetailedMessage.cs
@@ -1,29 +1,10 @@
-using System;
 using System.Text.Json.Serialization;
 
 namespace Estimmo.Api.Entities
 {
-    public class DetailedMessage
+    public class DetailedMessage : SimpleMessage
     {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("email")]
-        public string Email { get; set; }
-
-        [JsonPropertyName("subject")]
-        public string Subject { get; set; }
-
         [JsonPropertyName("content")]
         public string Content { get; set; }
-
-        [JsonPropertyName("sent_on")]
-        public DateTime SentOn { get; set; }
-
-        [JsonPropertyName("isArchived")]
-        public bool IsArchived { get; set; }
     }
 }
