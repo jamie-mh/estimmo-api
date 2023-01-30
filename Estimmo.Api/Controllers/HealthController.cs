84d1e15d860cf822cccfad24e9a67fe53257f1cd
Jamie Hodgson <jamie-hodgson@live.com>
2023-01-30 17:43:31 +0100

    add health endpoint

---
+++ Estimmo.Api/Controllers/HealthController.cs
@@ -0,0 +1,37 @@
+using Estimmo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Estimmo.Api.Controllers
+{
+    [ApiController]
+    [Route("/health")]
+    public class HealthController : ControllerBase
+    {
+        private readonly EstimmoContext _context;
+
+        public HealthController(EstimmoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(
+            Summary = "Check API health",
+            OperationId = "GetHealth",
+            Tags = new[] { "Health" }
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "API is available")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, "API is unavailable")]
+        public async Task<IActionResult> Default()
+        {
+            var canConnect = await _context.Database.CanConnectAsync();
+            return canConnect
+                ? Ok()
+                : StatusCode((int) HttpStatusCode.ServiceUnavailable);
+        }
+    }
+}
