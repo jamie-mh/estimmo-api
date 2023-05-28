42db6461542bea920f5d0bfd27a3890b72938b59
Jamie Hodgson <jamie-hodgson@live.com>
2023-05-28 10:33:35 +0200

    download latest available files

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ set -e
 
 REQUIREMENTS="dotnet curl gzip mapshaper"
 DEPARTMENT_IDS="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 21 22 23 24 25 26 27 28 29 2A 2B 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95"
-DVF_YEARS="2016 2017 2018 2019 2020 2021 2022"
 DOTNET_ARGS="--configuration Release --project Estimmo.Runner/Estimmo.Runner.csproj"
 
 
@@ -73,8 +72,9 @@ function download {
     fi
 
     mkdir -p download/sales
+    dvf_years=$(curl -vs https://files.data.gouv.fr/geo-dvf/latest/csv/ 2>&1 | sed -rn 's/^<a href="([0-9]{4}).*/\1/p')
 
-    for year in $DVF_YEARS; do
+    for year in $dvf_years; do
         if [[ -f "download/sales/$year.csv" ]]; then
             continue
         fi
