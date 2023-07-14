a4228b0dd74c41c52c37e33a65f60af87f7cb6d5
Jamie Hodgson <jamie-hodgson@live.com>
2023-07-14 14:11:53 +0200

    remove unused script

--- Estimmo.Api/watch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(dirname $(realpath -s "$0"))
-CSPROJ_PATH=$(realpath -s "$SCRIPT_DIR/Estimmo.Api.csproj")
-
-export DOTNET_ENVIRONMENT=Development
-dotnet watch run --project "$CSPROJ_PATH"
-
